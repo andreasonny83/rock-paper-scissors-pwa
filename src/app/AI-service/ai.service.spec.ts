a1e3c8f73feba4e209b64be9cdf9495e857b2fb3
Andrea SonnY <andreasonny83@gmail.com>
2017-06-07 13:39:32 +0100

    Feature/ai.service (#1)
    * test(ai.service): Generating testing cases for checkVictory
    
    * feature(ai.service): AIService finalised

--- src/app/AI-service/ai.service.spec.ts
+++ src/app/AI-service/ai.service.spec.ts
@@ -1,6 +1,7 @@
-import { TestBed, inject } from '@angular/core/testing';
+import { TestBed, inject, async, fakeAsync } from '@angular/core/testing';
 
 import { AIService } from './ai.service';
+import { Observable } from 'rxjs/Observable';
 
 describe('AIService', () => {
   beforeEach(() => {
@@ -12,4 +13,89 @@ describe('AIService', () => {
   it('should be created', inject([AIService], (service: AIService) => {
     expect(service).toBeTruthy();
   }));
+
+  it('should generate a random move', inject([AIService], (service: AIService) => {
+    const aiMove: IPlayerChose[] = ['Rock', 'Paper', 'Scissors'];
+    let reply;
+
+    service.generateRndmReply();
+    reply = service.aiMove;
+    expect(aiMove).toContain(reply);
+
+    service.generateRndmReply();
+    reply = service.aiMove;
+    expect(aiMove).toContain(reply);
+
+    service.generateRndmReply();
+    reply = service.aiMove;
+    expect(aiMove).toContain(reply);
+  }));
+
+  describe('checkVictory', () => {
+    let result: number;
+
+    it('should generate a tie when the player and the ai generated the same guess',
+    inject([AIService], (service: AIService) => {
+      service.aiMove = 'Rock';
+
+      result = service.checkVictory('Rock');
+      expect(result).toBe(0);
+
+      result = service.checkVictory('Paper');
+      expect(result).not.toBe(0);
+    }));
+
+    it('rock should win over scissors', inject([AIService], (service: AIService) => {
+      service.aiMove = 'Scissors';
+
+      result = service.checkVictory('Rock');
+      expect(result).toBe(1);
+    }));
+
+    it('rock should loose over paper', inject([AIService], (service: AIService) => {
+      service.aiMove = 'Paper';
+
+      result = service.checkVictory('Rock');
+      expect(result).toBe(-1);
+    }));
+
+    it('paper should win over rock', inject([AIService], (service: AIService) => {
+      service.aiMove = 'Rock';
+
+      result = service.checkVictory('Paper');
+      expect(result).toBe(1);
+    }));
+
+    it('paper should loose over scissors', inject([AIService], (service: AIService) => {
+      service.aiMove = 'Scissors';
+
+      result = service.checkVictory('Paper');
+      expect(result).toBe(-1);
+    }));
+
+    it('scissors should win over paper', inject([AIService], (service: AIService) => {
+      service.aiMove = 'Paper';
+
+      result = service.checkVictory('Scissors');
+      expect(result).toBe(1);
+    }));
+
+    it('scissors should loose over rock', inject([AIService], (service: AIService) => {
+      service.aiMove = 'Rock';
+
+      result = service.checkVictory('Scissors');
+      expect(result).toBe(-1);
+    }));
+  });
+
+  describe('Play', () => {
+    it('it should return the result of the game', async(inject([AIService], (service: AIService) => {
+      service.play('Rock')
+        .subscribe(res => {
+          expect(res['result']).toBeDefined();
+          expect(res['ai']).toBeDefined();
+          expect(res['message']).toBeDefined();
+        });
+    })));
+  });
 });
--- src/app/AI-service/ai.service.ts
+++ src/app/AI-service/ai.service.ts
@@ -1,26 +1,68 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
-import 'rxjs/observable/of';
 
 @Injectable()
 export class AIService {
+  aiMove: IPlayerChose;
+
   constructor() { }
 
   public play(playerMove: IPlayerChose): Observable<any> {
     return new Observable((observer) => {
       // Create a fake delay for increasing the suspense!
       setTimeout(() => {
-        this.generateReply(playerMove);
-        observer.next('data to send can be objet or anything');
+        this.generateRndmReply();
+        const result = this.checkVictory(playerMove);
+        observer.next({
+          result: result,
+          ai: this.aiMove,
+          message: this.generateMessage(result),
+        });
+
         observer.complete();
       }, 2000);
     });
   }
 
-  private generateReply(playerMove: IPlayerChose) {
-    const aiMove: IPlayerChose[] = ['Rock', 'Paper', 'Scissors'];
-    const chose: number = Math.floor(Math.random() * 3) + 1;
+  public generateRndmReply(): void {
+    const aiOptions: IPlayerChose[] = ['Rock', 'Paper', 'Scissors'];
+    const chose: number = Math.floor(Math.random() * 3);
 
-    return aiMove[chose];
+    this.aiMove = aiOptions[chose];
+  }
+
+  public checkVictory(playerMove: IPlayerChose): number  {
+    let gameResult;
+
+    if (playerMove === this.aiMove) {
+      return 0;
+    }
+
+    switch(playerMove) {
+      case 'Rock':
+        gameResult = this.aiMove === 'Scissors' ? 1 : -1;
+        break;
+      case 'Paper':
+        gameResult = this.aiMove === 'Rock' ? 1 : -1;
+        break;
+      case 'Scissors':
+        gameResult = this.aiMove === 'Paper' ? 1 : -1;
+        break;
+      default: gameResult = -1;
+    }
+
+    return gameResult;
+  }
+
+  private generateMessage(outcome: number) {
+    if (outcome === 0) {
+      return 'That was a tie!';
+    }
+
+    if (outcome === 1) {
+      return `You're the winner!`;
+    }
+
+    return `You have been defeated!`;
   }
 }
