3d706092d11cf9a6cd4d369b0e8f6f7430ddba09
Andrea Sonny <andreasonny83@gmail.com>
2017-06-07 14:38:14 +0100

    test(Protractor): E2E tests improved

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,3 +1,4 @@
+import { browser, element, by, ElementFinder } from 'protractor';
 import { RockPaperScissorsPwaPage } from './app.po';
 
 describe('Rock Paper Scissors App', () => {
@@ -5,12 +6,50 @@ describe('Rock Paper Scissors App', () => {
 
   beforeEach(() => {
     page = new RockPaperScissorsPwaPage();
+    page.navigateTo();
   });
 
   it('should display welcome message', done => {
-    page.navigateTo();
     page.getParagraphText()
       .then(msg => expect(msg).toEqual('Welcome to Rock Paper Scissors!!'))
+      .then(done, done.fail);
+  });
+
+  it('should display a md-radio-group element', done => {
+    element(by.css('.mat-radio-group')).isPresent()
+      .then(isPresent => expect(isPresent).toBe(true))
+      .then(done, done.fail);
+  });
+
+  it('start button should be disabled until the user chose a move', done => {
+    element(by.css('.game_start')).getAttribute('disabled')
+      .then(disabled => expect(disabled).toBe('true'))
+      .then(done, done.fail);
+  });
+
+  it('on chosing a move the play button should become active', done => {
+    element(by.css('#md-radio-1')).click();
+
+    element(by.css('.game_start')).getAttribute('disabled')
+      .then(disabled => expect(disabled).toBe(null))
+      .then(done, done.fail);
+  });
+
+  it('on submitting a move, the app should display a dialog', done => {
+    element(by.css('#md-radio-1')).click();
+    element(by.css('.game_start')).click();
+
+    element(by.css('.mat-dialog-container')).isPresent()
+      .then(isPresent => expect(isPresent).toBe(true))
+      .then(done, done.fail);
+  });
+
+  it('and it should contain the match result', done => {
+    element(by.css('#md-radio-1')).click();
+    element(by.css('.game_start')).click();
+
+    element(by.css('.mat-dialog-container .mat-dialog-content')).getText()
+      .then(text => expect(/^The opponent replied with/g.test(text)).toBe(true))
       .then(done, done.fail);
   });
 });
--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -1,11 +1,23 @@
-import { browser, by, element } from 'protractor';
+import { browser, element, by, ElementFinder } from 'protractor';
 
 export class RockPaperScissorsPwaPage {
+  private timeout: number;
+
   navigateTo() {
     return browser.get('/');
   }
 
   getParagraphText() {
     return element(by.css('app-root h1')).getText();
+  }
+
+  waitForElement(el: ElementFinder): any {
+    const timeout: number = this.timeout;
+
+    return browser.driver
+      .wait(() => el.isPresent(), timeout)
+      .then(() => {
+         return browser.driver.wait(() => el.isDisplayed(), timeout);
+      });
   }
 }
